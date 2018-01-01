ce691d7e8c82a01447e3ab67a6d2f6adf5b2bf72
Stewart ilondanga <stilondanga@gmail.com>
2018-01-01 23:10:56 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -28,6 +28,63 @@ var multipleSupport = typeof $('<input/>')[0].multiple !== 'undefined',
 		 $wrap.insertAfter( $file )
 				 .append( $file, $input, ( isIE ? $label : $button ) );
 
+				 // Prevent focus
+	 		      $file.attr('tabIndex', -1);
+	 		      $button.attr('tabIndex', -1);
+
+	 		      $button.click(function () {
+	 		        $file.focus().click(); // Open dialog
+	 		      });
+
+	 		      $file.change(function() {
+
+	 		        var files = [], fileArr, filename;
+
+	 		        // If multiple is supported then extract
+	 		        // all filenames from the file array
+	 		        if ( multipleSupport ) {
+	 		          fileArr = $file[0].files;
+	 		          for ( var i = 0, len = fileArr.length; i < len; i++ ) {
+	 		            files.push( fileArr[i].name );
+	 		          }
+	 		          filename = files.join(', ');
+
+	 		        // If not supported then just take the value
+	 		        // and remove the path to just show the filename
+	 		        } else {
+	 		          filename = $file.val().split('\\').pop();
+	 		        }
+
+	 		        $input.val( filename ) // Set the value
+	 		          .attr('title', filename) // Show filename in title tootlip
+	 		          .focus(); // Regain focus
+
+	 		      });
+
+	 		      $input.on({
+	 		        blur: function() { $file.trigger('blur'); },
+	 		        keydown: function( e ) {
+	 		          if ( e.which === 13 ) { // Enter
+	 		            if ( !isIE ) { $file.trigger('click'); }
+	 		          } else if ( e.which === 8 || e.which === 46 ) { // Backspace & Del
+	 		            // On some browsers the value is read-only
+	 		            // with this trick we remove the old input and add
+	 		            // a clean clone with all the original events attached
+	 		            $file.replaceWith( $file = $file.clone( true ) );
+	 		            $file.trigger('change');
+	 		            $input.val('');
+	 		          } else if ( e.which === 9 ){ // TAB
+	 		            return;
+	 		          } else { // All other keys
+	 		            return false;
+	 		          }
+	 		        }
+	 		      });
+
+	 		    });
+
+	 		  };
+
 
 
 var navigate = (function() {
